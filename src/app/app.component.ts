25c7c031d38c33cc2ded3568ced5cbda181c10b3
ganatan <ganatanbriel@gmail.com>
2019-04-19 08:38:37 +0200

    update angular 7.2.13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-starter';
+
+  constructor() {
+    console.log('Constructor:AppComponent');
+  }
 }
--- src/app/modules/not-found/not-found.component.ts
+++ src/app/modules/not-found/not-found.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('Constructor:NotFoundComponent');
+  }
+
 
   ngOnInit() {
   }
