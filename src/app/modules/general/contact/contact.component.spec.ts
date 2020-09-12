3cc5dfd1693a419ee58e9a1a8e64c341768a602e
ganatan <dannyganatan@gmail.com>
2020-09-12 17:51:12 +0200

    Update Angular 10.1.1 & Spec files

--- src/app/modules/general/contact/contact.component.spec.ts
+++ src/app/modules/general/contact/contact.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactComponent } from './contact.component';
 
@@ -6,12 +6,12 @@ describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [ ContactComponent ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ContactComponent);
