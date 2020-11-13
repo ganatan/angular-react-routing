1ec7d4e832e8f35ed00446054002f175190b8216
ganatan <dannyganatan@gmail.com>
2020-11-13 07:13:48 +0100

    Update Angular 11.0.0

--- src/app/modules/general/contact/contact.component.ts
+++ src/app/modules/general/contact/contact.component.ts
@@ -7,12 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() {
-  }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
 }
-
-
