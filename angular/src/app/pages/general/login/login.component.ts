51a85aeda2425ccf9bc32ce820904ab95c2cc8fd
ganatan <dannyganatan@gmail.com>
2023-12-07 14:39:12 +0100

    Update Angular 17.0.6

--- angular/src/app/pages/general/login/login.component.ts
+++ angular/src/app/pages/general/login/login.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- angular/src/app/pages/general/signup/signup.component.ts
+++ angular/src/app/pages/general/signup/signup.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
